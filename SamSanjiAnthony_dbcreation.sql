746fda051d0406a6afa61e7d9060a0203a4da5dc
Sam Gaudet <sgaudet1@babson.edu>
2018-03-15 20:53:41 -0400

    Deleted copy/psated code

--- SamSanjiAnthony_dbcreation.sql
+++ SamSanjiAnthony_dbcreation.sql
@@ -26,39 +26,3 @@ CREATE TABLE SalesReceipt(
 	TaxAmount money NOT NULL,
 	TotalDue AS Subtotal + TaxAmount);
 
-
----- Create the relationship: the first FK in CourseEnrollment
---ALTER TABLE CourseEnrollment ADD CONSTRAINT FK_CourseEnrollment_Student 
---FOREIGN KEY (StudentID) REFERENCES Student(StudentID);
---GO
-
---ALTER TABLE CourseEnrollment ADD CONSTRAINT FK_CourseEnrollment_Course 
---FOREIGN KEY (CourseID) REFERENCES Course(CourseID);
---GO
-
----- Add a few students
---INSERT INTO Student (StudentID, FirstName, LastName) VALUES 
---(1, 'Yinmeng', 'Tian'),
---(2, 'Samuel', 'Gaudet'),
---(3, 'Anthony', 'Krichevskiy'),
---(4, 'Christian', 'Thompson'),
---(5, 'Sang Won', 'Baek'),
---(6, 'Matthew', 'Michalke'),
---(7, 'Jonathan', 'Marchetti'),
---(8, 'Edward', 'Lehr'),
---(9, 'Patrick', 'Carney'),
---(10, 'Emma', 'Sewell');
-
----- Add a few courses
---INSERT INTO Course(CourseID, CourseTitle, Instuctor) VALUES 
---(1, 'MIS3545-BUSINESS INTELLIGENCE AND DATA ANALYTICS', 'Zhi'),
---(2, 'MIS3690-WEB TECHNOLOGIES', 'Shankar'),
---(3, 'QTM3000-CASE STUDIES IN BUSINESS ANALYTICS', 'Nathan'),
---(4, 'QTM3625-FINANCIAL SIMULATION', 'Dessi'),
---(5, 'SME2012-MANAGING INFORMATION TECH AND SYSTEMS', 'Clare');
---GO
-
----- If we want to delete SME2012
---DELETE FROM Course
---WHERE CourseTitle like 'SME%';
---GO
