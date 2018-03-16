678fd1042fd668c4ed5974f5d7ac103abb05d6e1
Sam Gaudet <sgaudet1@babson.edu>
2018-03-15 20:54:48 -0400

    Fixed spacing

--- SamSanjiAnthony_dbcreation.sql
+++ SamSanjiAnthony_dbcreation.sql
@@ -6,15 +6,15 @@ GO
 
 -- Create the Customer Table
 CREATE TABLE Customer(
-  CustomertID bigint NOT NULL PRIMARY KEY,
-  FirstName varchar(50) NOT NULL,
-  LastName varchar(50) NOT NULL,
-  Email varchar(50) NOT NULL,
-  Phone char(10) NOT NULL,
-  StreetAddress varchar(50) NOT NULL,
-  City varchar(50),
-  State varchar(20) NOT NULL,
-  ZipCode char(10));
+	CustomertID bigint NOT NULL PRIMARY KEY,
+	FirstName varchar(50) NOT NULL,
+	LastName varchar(50) NOT NULL,
+	Email varchar(50) NOT NULL,
+	Phone char(10) NOT NULL,
+	StreetAddress varchar(50) NOT NULL,
+	City varchar(50),
+	State varchar(20) NOT NULL,
+	ZipCode char(10));
 
 -- Create SalesReceipt Table
 CREATE TABLE SalesReceipt(
